a677e56d3548f7c0bb07588cd119c3f12b7e9c7b
rud <rudvadim@gmail.com>
2017-07-05 15:52:00 +0300

    add label

---
+++ main.py
@@ -0,0 +1,20 @@
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+from kivy.core.text import LabelBase
+LabelBase.register(name='Roboro', fn_regular='fonts/Roboto-Regular.ttf', fn_bold='fonts/Roboto-Bold.ttf',
+                   fn_italic='fonts/Roboto-Italic.ttf')
+
+
+class ClockForm(BoxLayout):
+    pass
+#    pass
+
+
+class ClockApp(App):
+    kv_directory = 'templates'
+
+
+if __name__ == '__main__':
+    ClockApp().run()
